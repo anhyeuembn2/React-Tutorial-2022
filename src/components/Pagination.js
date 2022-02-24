9e72e36644875125bc77b6a60bfcf79a11d7fd91
devat-youtuber <battuosai01@gmail.com>
2022-02-24 18:39:56 +0700

    10 Finish Pagination & usePagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,35 +1,28 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import usePagination from '../hooks/usePagination'
 
 const Pagination = ({totalPages, page}) => {
-  const newArr = [...Array(totalPages)].map((_, i) => i + 1)
-  const navigate = useNavigate()
-
-  const isActive = (index) => {
-    if(index === page) return "active"
-    return ""
-  }
-
-  const prev = () => {
-    const newPage = Math.max(page - 1, 1)
-    navigate(`?page=${newPage}`)
-  }
-
-  const next = () => {
-    const newPage = Math.min(page + 1, totalPages)
-    navigate(`?page=${newPage}`)
-  }
-
-  const jump = (num) => {
-    navigate(`?page=${num}`)
-  }
+  const { 
+    firstArr, lastArr, isActive, prev, next, jump 
+  } = usePagination(totalPages, page)
 
   return (
     <div className='pagination'>
       <button onClick={prev}>&laquo;</button>
 
       {
-        newArr.map(num => (
+        firstArr.map(num => (
+          <button key={num} className={`${isActive(num)}`}
+          onClick={() => jump(num)}>
+            {num}
+          </button>
+        ))
+      }
+
+      { lastArr.length > 0 && <button>...</button> }
+      
+      {
+        lastArr.map(num => (
           <button key={num} className={`${isActive(num)}`}
           onClick={() => jump(num)}>
             {num}
